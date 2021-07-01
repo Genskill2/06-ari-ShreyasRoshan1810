4a87c2970e776a53fd932afa9965c09c81abfaae
ShreyasRoshan1810 <84716136+ShreyasRoshan1810@users.noreply.github.com>
2021-07-01 09:22:47 +0530

    Update main.c

--- main.c
+++ main.c
@@ -2,29 +2,32 @@
 #include <assert.h>
 #include <string.h>
 #include <cs50.h>
+#include <math.h>
+#include <ctype.h>
 
-string ari(string);
-
-
-void test1() {
-  string s1="The town was large enough to support three inns, and all their common rooms were rife with rumors. Arianne sent her men into each of them, to hear what they might hear. In the Broken Shield, Daemon Sand was told that the great septry on the Holf of Men had been burned and looted by raiders from the sea, and a hundred young novices from the motherhouse on Maiden Isle carried off into slavery.";
-  assert(strcmp(ari(s1), "Eleventh Grade") == 0);
-}
-
-void test2() { 
- string s2="A mouse took a stroll through the deep dark wood. A fox saw the mouse, and the mouse looked good. Where are you going to, little brown mouse? Come and have lunch in my underground house.";
-  assert(strcmp(ari(s2), "Third Grade") == 0);
-}
-
-void test3() {
-  string s3="Exhaled respiratory particles can build-up within enclosed spaces with inadequate ventilation. The risk of COVID-19 infection increases especially in spaces where people engage in physical exertion or raise their voice (e.g., exercising, shouting, singing) as this increases exhalation of respiratory droplets. Prolonged exposure to these conditions, typically more than 15 minutes, leads to higher risk of infection.";
-  assert(strcmp(ari(s3), "Professor") == 0);
+void swap_max(int arr[],int l,int n)
+{
+ int max=arr[n];
+ int index=n;
+ int i;
+ for(i=n;i<l;i++)
+ {
+  if(arr[i]>max)
+  {
+    max=arr[i];
+    index=i;
+  }
  }
-
-int main(void) {
-  test1();
-  test2();
-  test3();
+ int temp;
+ temp=arr[n];
+ arr[n]=max;
+ arr[index]=temp;
 }
 
 
+void ssort(int arr[],int l)
+{
+ int  i;
+ for(i=0;i<l;i++)
+   swap_max(arr,l,i);
+}
