a66d34c594eee8af3c12815006e0e1bc20034572
ShreyasRoshan1810 <84716136+ShreyasRoshan1810@users.noreply.github.com>
2021-07-01 09:59:37 +0530

    Update main.c

--- main.c
+++ main.c
@@ -5,29 +5,114 @@
 #include <math.h>
 #include <ctype.h>
 
-void swap_max(int arr[],int l,int n)
+string ari(string s)
 {
- int max=arr[n];
- int index=n;
  int i;
- for(i=n;i<l;i++)
+ float cha=0.000000;
+ float words=0.000000;
+ float sentence=0.000000;
+ for(i=0;s[i]!='\0';i++)
  {
-  if(arr[i]>max)
+  if(isalnum(s[i]))
   {
-    max=arr[i];
-    index=i;
+    ++cha;
   }
  }
- int temp;
- temp=arr[n];
- arr[n]=max;
- arr[index]=temp;
+
+  for(i=0;s[i]!='\0';i++)
+ {
+  if(s[i]=='.' || s[i]=='?' || s[i]=='!')
+  {
+    ++sentence;
+  }
+ }
+
+  for(i=0;s[i]!='\0';i++)
+ {
+  if(s[i]==' ')
+  {
+    ++words;
+  }
+ }
+
+ 
+ float result;
+ result= (4.71*cha/words) + (0.5*words/sentence) - 21.43;
+ int x;
+ string w;
+ x=ceil(result);
+ if(x==1)
+ {
+  w= "Kindergarden";
+  return w;
+ }
+ if(x==2)
+ {
+  w="First/Second Grade";
+  return w;
+ }
+ if(x==3)
+ {
+  w="Third Grade";
+  return w;
+ }
+ if(x==4)
+ {
+ w="Fourth Grade";
+ return w;
 }
+ if(x==5)
+  {
+ w="Fifth Grade";
+ return w;
+}
+ if(x==6)
+  {
+ w="Sixth Grade";
+ return w;
+}
+ if(x==7)
+  {
+ w="Seventh Grade";
+ return w;
+}
+ if(x==8)
+  {
+ w="Eigth Grade";
+ return w;
+}
+ if(x==9)
+ {
+ w="Ninth Grade";
+ return w;
+}
+ if(x==10)
+  {
+ w="Tenth Grade";
+ return w;
+}
+ if(x==11)
+  {
+ w="Eleventh Grade";
+ return w;
+}
+ if(x==12)
+ {
+ w="Twelfth Grade";
+ return w;
+}
+ if(x==13)
+ {
+ w="College student";
+ return w;
+}
+ if(x==14)
+ {
+ w="Professor";
+ return w;
+}
+ 
+ 
 
 
-void ssort(int arr[],int l)
-{
- int  i;
- for(i=0;i<l;i++)
-   swap_max(arr,l,i);
 }
